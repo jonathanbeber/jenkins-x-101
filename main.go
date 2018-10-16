fce8505e0d43a71bbba4486a25ddcd8b7922a56a
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-10-16 22:24:59 +0200

    first simple version

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello, world!"))
+}
+
+func main(){
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe("0.0.0.0:8000", nil); err != nil{
+		panic(err)
+	}
+}
